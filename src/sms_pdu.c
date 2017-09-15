9a2ecee01144204582246f9a9e125abbfee4a15b
Mlen-Too Wesley <mlen.too.wesley@gmail.com>
2017-09-15 16:01:09 +0000

    handling weird GSM-7 encoding

--- src/sms_pdu.c
+++ src/sms_pdu.c
@@ -19,6 +19,28 @@ static inline char hex2c(uint8_t d)
     return hexchar[d];
 }
 
+static const char gsm7_ascii[] = {
+    '@', ' ', '$', ' ', 'e', 'e', 'u', 'i', 'o', 'C', '\n', '0', '0', '\r', 'A', 'a', 
+    ' ', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '\e', 'A', 'a', 'B', 'E', 
+    ' ', '!', '“', '#', ' ', '%', '&', '‘', '(', ')', '*', '+', ',', '-', '.', '/', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', 
+    '!', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'O', 'N', 'U', ' ', 
+    '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'o', 'n', 'u', 'a' 
+};
+
+static const char ascii_gsm7[] = {
+    0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x0A, 0x20, 0x20, 0x0D, 0x20, 0x20, 
+    0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x1B, 0x20, 0x20, 0x20, 0x20, 
+    0x20, 0x21, 0x22, 0x23, 0x02, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 
+    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 
+    0x00, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 
+    0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x20, 0x20, 0x20, 0x20, 0x11, 
+    0x20, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 
+    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x20, 0x20, 0x20, 0x20, 0x20
+}
+
 /*
  * http://twit88.com/home/utility/sms-pdu-encode-decode
  */
@@ -32,7 +54,7 @@ static int decode7(uint8_t *in, size_t in_sz, uint8_t *out, size_t out_sz, uint8
     }
 
     if ( padd ) {
-        out[0] = in[0] >> 1;
+        out[0] = gsm7_ascii[(int)(in[0] >> 1)];
         i++; j++;        
     }
     
@@ -40,7 +62,7 @@ static int decode7(uint8_t *in, size_t in_sz, uint8_t *out, size_t out_sz, uint8
     for ( shift = 8; (i < in_sz) && (j < in_sz); ++i, ++j, --shift ) {
 
         if ( shift == 1 ) {
-            out[j] = (prev >> 1);
+            out[j] = gsm7_ascii[(int)(prev >> 1)];
             ++j;
             shift = 8;
         }
@@ -50,7 +72,7 @@ static int decode7(uint8_t *in, size_t in_sz, uint8_t *out, size_t out_sz, uint8
         new <<= (8 - shift);
         new |= ((prev & (0xFF << shift)) >> shift);
         new &= ~0x80;
-        out[j] = new;
+        out[j] = gsm7_ascii[(int)new];
 
         prev = in[i];
     }
@@ -68,7 +90,7 @@ static int encode7(uint8_t *in, size_t in_sz, uint8_t *out, size_t out_sz, uint8
     }
 
     if ( padd ) {
-        out[0] = in[0] << 1;
+        out[0] = ascii_gsm7[(int)(in[0] << 1)];
         i++; j++;
     }
     
@@ -84,7 +106,7 @@ static int encode7(uint8_t *in, size_t in_sz, uint8_t *out, size_t out_sz, uint8
             new |= ((next & (0xFF >> shift)) << shift);
         }
 
-        out[j] = new;
+        out[j] = ascii_gsm7[(int)new];
 
         if ( shift == 1 ) {
             ++i;
